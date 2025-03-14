a8af3bca9f080bfbee612fece6a1517d71fe3966
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-03-14 14:24:06 +0000

    update

--- syvox_backend/TTS/urls.py
+++ syvox_backend/TTS/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('tts/', views.gen_tts),
+    path('jobs/', views.fetch_jobs),
 ]
--- syvox_backend/TTS/views.py
+++ syvox_backend/TTS/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from gtts import gTTS
+from .models import TTSJob
 import os
 
 def index(request):
@@ -15,3 +16,17 @@ def gen_tts(request):
             tts.save(file_path)
             return render(request, 'index.html', {'audio_file':'output.mp3'})
     return HttpResponse("Error:No text provided")
+
+def fetch_jobs(request):
+    jobs = TTSJob.objects.all().order_by('-created_date')
+    job_list = []
+    for job in jobs:
+        job_list.append({
+            'job_name': job.job_name,
+            'description' : job.description,
+            'created_date' : job.created_data.strftime('$Y-$m-$d $H:$M:$S'),
+            'file_location' : job.file_location,
+            'download_link' : job.download_link,
+            'status' : job.status,
+        })
+    return JsonResponse({'jobs':job_list})
