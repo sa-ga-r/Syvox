d5e394013163e3b3a046a79ddd8f6de514029859
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-03-14 09:35:26 +0000

    update

--- syvox_backend/TTS/models.py
+++ syvox_backend/TTS/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class TTSJob(models.Model):
+    job_name = models.CharField(max_length=150)
+    description = models.TextField()
+    created_data = models.DateTimeField(auto_now_add=True)
+    file_location = models.CharField(max_length=255)
+    download_link = models.CharField(max_length=255, blank=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.job_name
