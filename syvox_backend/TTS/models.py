581e4257afeb17ff63b4a7e2311dfc429e70b876
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-03-14 14:39:25 +0000

    update

--- syvox_backend/TTS/models.py
+++ syvox_backend/TTS/models.py
@@ -4,9 +4,9 @@ class TTSJob(models.Model):
     job_name = models.CharField(max_length=150)
     description = models.TextField()
     created_data = models.DateTimeField(auto_now_add=True)
-    file_location = models.CharField(max_length=255, blank=True)
-    download_link = models.CharField(max_length=255, blank=True)
-    status = models.BooleanField(default=False)
+    file_location = models.CharField(max_length=255, blank=True, null=True)
+    download_link = models.CharField(max_length=255, blank=True, null=True)
+    status = models.CharField(max_length=20, default='PENDING')
 
     def __str__(self):
         return self.job_name
