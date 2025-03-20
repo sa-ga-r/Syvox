c9462f10d5d7ca0189dacc6e5d986a2b98abf8e7
sa-ga-r B <sagar.bhalerao@yahoo.co.in>
2025-03-20 13:49:47 +0000

    update

--- syvox_backend/TTS/views.py
+++ syvox_backend/TTS/views.py
@@ -69,11 +69,11 @@ def gen_tts(request, job_id):
             return JsonResponse({'status':'error', 'message':'Description is empty.'})
         tts = gTTS(text=text, lang='en')
         file_name = f"{job.job_name.replace(' ', '_')}_{job_id}.mp3"
-        file_path = os.path.join('/static/')
+        file_path = os.path.join('TTS/static/')
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         tts.save(file_path)
         job.file_location=os.path.join(file_path, file_name)
         job.status='DONE'
         job.save()
-        return JsonResponse({'status':'success', 'audio_file':job.file_location, 'file_location':job.file_location})
+        return JsonResponse({'status':'success', 'audio_file':job.file_name, 'file_location':job.file_name})
     return JsonResponse({'status':'error', 'message':'Invalid request method'})
