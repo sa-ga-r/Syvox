fc103e915a0c4363502715518808ed5a1f4e9367
sa-ga-r B <sagar.bhalerao@yahoo.co.in>
2025-04-04 14:25:32 +0000

    update

--- syvox_backend/STT/models.py
+++ syvox_backend/STT/models.py
@@ -5,9 +5,9 @@ class STTJob(models.Model):
     description = models.TextField(blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     file_location = models.CharField(max_length=150, default="N/A")
-    download_link = models.CharField(blank=True, null=True, default="N/A")
+    download_link = models.CharField(max_length=150, default="N/A")
     status = models.CharField(max_length=20, default='PENDING')
-    text_file = models.TextField(blank=True, default='N/A')
+    text_file = models.CharField(blank=True, default='N/A')
 
     def __str__(self):
         return self.job_name
