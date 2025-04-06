d8f5de75c864d1d56f7b6bfbfd55cf8ae7f4224e
sa-ga-r B <sagar.bhalerao@yahoo.co.in>
2025-04-06 16:00:54 +0000

    update

--- syvox_backend/STT/urls.py
+++ syvox_backend/STT/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('stt/<int:job_id>/', views.gen_tts),
+    path('jobs/', views.fetch_jobs),
+    path('create_job/', views.create_job),
+    path('delete_job/<int:job_id>/', views.delete_job),
 ]
