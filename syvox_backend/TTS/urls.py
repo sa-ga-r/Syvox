fb162fb2c389eb1b0ba67aa718232fb9b6bb26c4
sa-ga-r B <sagar.bhalerao@yahoo.co.in>
2025-04-07 15:12:10 +0000

    update

--- syvox_backend/TTS/urls.py
+++ syvox_backend/TTS/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('TTS/', views.index1, name='index1'),
     path('tts/<int:job_id>/', views.gen_tts),
-    path('jobs/', views.fetch_jobs),
-    path('create_job/', views.create_job),
-    path('delete_job/<int:job_id>/', views.delete_job),
+    path('TTS/jobs/', views.fetch_jobs),
+    path('TTS/create_job/', views.create_job),
+    path('TTS/delete_job/<int:job_id>/', views.delete_job),
 ]
