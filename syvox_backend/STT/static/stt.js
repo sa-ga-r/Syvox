aa7018279cc6c885656a36d411db1b8da78c273d
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-04-10 06:42:16 +0000

    update

--- syvox_backend/STT/static/stt.js
+++ syvox_backend/STT/static/stt.js
@@ -92,9 +92,9 @@ function gen_stt(jobId){
     .then(data => {
         if (data.status === 'success'){
             document.getElementById(`status-${jobId}`).innerText='DONE';
-            document.getElementById(`file-location-${jobId}`).innerText=data.file_location;
+            /*document.getElementById(`file-location-${jobId}`).innerText=data.file_location;*/
             document.getElementById(`download-link-${jobId}`).innerHTML=`<a href="${data.download_url}" download>Download</a>`;
-            document.getElementById(`audio-player-${jobId}`).innerHTML='<div class="preview-text">${data.preview}</div>';
+            document.getElementById(`preview-${jobId}`).innerHTML='<div class="preview-text">${data.preview}</div>';
         } else {
             alert(`Error processing STT: ${data.message}`);
         }
