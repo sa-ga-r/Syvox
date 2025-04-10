fcdd44280af1916955ae7e4cd2713443a5e72969
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-04-10 11:41:50 +0000

    update

--- syvox_backend/STT/views.py
+++ syvox_backend/STT/views.py
@@ -113,7 +113,9 @@ def gen_stt(request, job_id):
             txt_path = os.path.join(static_dir, txt_filename)
             with open(txt_path, 'w', encoding='utf-8') as text_file:
                 text_file.write(stt)
-            preview = stt[:10] + '...' if len(stt) > 10 else stt
+            with open(text_file, 'r', encoding='utf-8') as text_snip:
+                snip_priview = text_file.read(100)
+            preview = snip_priview[:10] + '...' if len(snip_priview) > 10 else snip_priview
             job.text_file = txt_filename
             job.download_link = f"/static/{txt_filename}"
             job.status = "DONE"
