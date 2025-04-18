5f072c821990334a07ae7db5f80a1e5f783bd130
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-04-18 12:03:32 +0000

    update

--- syvox_backend/syvox_backend/urls.py
+++ syvox_backend/syvox_backend/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('TTS/', include('TTS.urls')),
     path('', include('STT.urls')),
+    path('TTS/', include('TTS.urls')),
+    path('STT/', include('STT.urls')),
 ]
