e72f98b793ae19f3605646ab273d457955de2cb7
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-04-18 11:49:28 +0000

    update

--- syvox_backend/STT/urls.py
+++ syvox_backend/STT/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('STT/', views.index, name='index'),
+    #path('STT/', views.index, name='index'),
     path('gen_stt/<int:job_id>/', views.gen_stt),
     path('stt_jobs/', views.stt_fetch_jobs),
     path('stt_create_job/', views.stt_create_job),
