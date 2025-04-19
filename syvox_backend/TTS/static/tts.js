5b1e650f6f15902daf2d3aa61d7bcc6fb318d16c
sa-ga-r B <sagar.bhalerao@yahoo.co.in>
2025-04-19 13:44:39 +0000

    update

--- syvox_backend/TTS/static/tts.js
+++ syvox_backend/TTS/static/tts.js
@@ -18,17 +18,17 @@ document.addEventListener('DOMContentLoaded', function () {
                 description: description
             })
         })
-        .then(response => response.json())
-        .then(data => {
-            if (data) {
-                alert('Job successfully created!');
-                fetchJobs();
-                form.reset();
-            } else {
-                alert('Error1: data creation failed');
-            }
-        })
-        .catch(error => console.error('Error2: Network error'));
+            .then(response => response.json())
+            .then(data => {
+                if (data) {
+                    alert('Job successfully created!');
+                    fetchJobs();
+                    form.reset();
+                } else {
+                    alert('Error: data creation failed');
+                }
+            })
+            .catch(error => console.error('Error: Network error'));
     })
 });
 
@@ -42,59 +42,60 @@ function fetchJobs() {
             data.jobs.forEach((job, index) => {
                 const row = document.createElement('tr');
                 row.innerHTML = `
-                    <td>${index + 1}</td>
-                    <td>${job.job_name}</td>
-                    <td>${job.description.substring(0, 20)}...</td>
-                    <td>${job.created_date}</td>
-                    <td id="file-location-${job.id}">${job.file_location || 'N/A'}</td>
-                    <td id="download-link-${job.id}">${job.file_location ? `<a href="${job.file_location}" download>Download</a>` : 'N/A'}</td>
-                    <td id="status-${job.id}">${job.status}</td>
-                    <td id="audio-player-${job.id}">${job.file_location && job.status === 'DONE' ?
+                                                                                                                                                                                                                                                                                                                                                                                                                        <td>${index + 1}</td>
+                                                                                                                                                                                                                                                                                                                                                                                                                                            <td>${job.job_name}</td>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                <td>${job.description.substring(0, 20)}...</td>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    <td>${job.created_date}</td>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        <td id="file-location-${job.id}">${job.file_location || 'N/A'}</td>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            <td id="download-link-${job.id}">${job.file_location ? `<a href="${job.file_location}" download>Download</a>` : 'N/A'}</td>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                <td id="status-${job.id}">${job.status}</td>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    <td id="audio-player-${job.id}">${job.file_location && job.status === 'DONE' ?
                         `<audio controls><source src="${job.audio_file}" type="audio/mp3">Your browser does not support the audio element.</audio>`
                         : 'N/A'}
-                    </td>
-                    <td>
-                    <button onclick="delete_job(${job.id})">Delete</button>
-                    <button onclick="gen_tts(${job.id})">Process</button>
-                    </td>
-                `;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        </td>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            <td>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                <button onclick="delete_job(${job.id})">Delete</button>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    <button onclick="gen_tts(${job.id})">Process</button>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        </td>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        `;
                 tableBody.appendChild(row);
             });
         })
         .catch(error => console.error('Error:', error));
 }
 
-
 function delete_job(jobId) {
-    fetch(`/tts_delete_job/${jobId}/`, {
-        method : 'DELETE',
-        headers : {
-            'Content-Type':'application/json',
-        }
-    })
-    .then(response => {
-        fetchJobs();
-    })
+    if (confirm("Are you sure you want to delete this job?")) {
+        fetch(`/tts_delete_job/${jobId}/`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(response => {
+                fetchJobs();
+            })
+    }
 }
 
-function gen_tts(jobId){
+function gen_tts(jobId) {
     fetch(`/tts/${jobId}/`, {
-        method : 'POST',
-        headers : {
-            'Content-Type':'application/json',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
         }
     })
-    .then(response => response.json())
-    .then(data => {
-        if (data.status === 'success'){
-            document.getElementById(`status-${jobId}`).innerText='DONE';
-            document.getElementById(`file-location-${jobId}`).innerText=data.file_location;
-            document.getElementById(`download-link-${jobId}`).innerHTML=`<a href="${data.file_location}" download>Download</a>`;
-            const audio_player = document.getElementById(`audio-player-${jobId}`).innerHTML=`<audio controls><source src="${data.file_location}" type="audio/mp3"> Your browser does not support audio element.</audio>`;
-            audio_player.src = data.file_location;
-        } else {
-            alert(`Error processing TTS: ${data.message}`);
-        }
-    })
-    .catch(error => console.error("Error generating TTS:", error));
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'success') {
+                document.getElementById(`status-${jobId}`).innerText = 'DONE';
+                document.getElementById(`file-location-${jobId}`).innerText = data.file_location;
+                document.getElementById(`download-link-${jobId}`).innerHTML = `<a href="${data.file_location}" download>Download</a>`;
+                const audio_player = document.getElementById(`audio-player-${jobId}`).innerHTML = `<audio controls><source src="${data.file_location}" type="audio/mp3"> Your browser does not support audio element.</audio>`;
+                audio_player.src = data.file_location;
+            } else {
+                alert(`Error processing TTS: ${data.message}`);
+            }
+        })
+        .catch(error => console.error("Error generating TTS:", error));
 }
